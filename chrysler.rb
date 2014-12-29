09480fd2e1401980d346edaad4ab9bc43650e0f2
Sean Rankin <sean.rankin@gmail.com>
2014-12-29 09:02:01 -0500

    Removed unused code

--- chrysler.rb
+++ chrysler.rb
@@ -1,24 +1,21 @@
+#!/usr/bin/env ruby
 # rails-app-template.rb
-# Usage: rails new app-name --database=postgresql -m ./chrysler/chrysler.rb
-# Usage remote: rails new app-name --database=postgresql -m https://raw.githubusercontent.com/seanrankin/chrysler/master/chrysler.rb
+# Usage: rails new app-name --database=postgresql -m https://raw.githubusercontent.com/seanrankin/chrysler/master/chrysler.rb
 
 # Get the path of the application template
 path = File.expand_path File.dirname(__FILE__)
 remotePath = "https://raw.githubusercontent.com/seanrankin/chrysler/master"
 
-# Do you want to deploy to Heroku?
 publish = yes? "Do you want to deploy to Heroku?"
 if publish
   appname = ask("What do you want to name this app?")
 end
 
-# Should we create a default controller?
 root_controller = yes? "Should we create a default controller?"
 if root_controller
   root_controller_name = ask("What is the root controllers name?").underscore
 end
 
-# Do you want to use HAML for templating?
 haml = yes? "Do you want to use HAML for templating?"
 if haml
   gem "haml"
@@ -79,9 +76,9 @@ run "touch .env"
 run "touch Procfile"
 run "touch Guardfile"
 
-# Create a default files
+# Create a default script file
 run "curl -o app/assets/javascripts/scripts.coffee #{remotePath}/scripts.coffee"
-run "curl -o app/views/layouts/application.html.erb #{remotePath}/application.html.erb"
+# run "curl -o app/views/layouts/application.html.erb #{remotePath}/application.html.erb"
 
 if haml
   run "rails generate haml:application_layout convert"
@@ -109,4 +106,3 @@ end
 run "atom ."
 
 run "rails s"
-# run "open http://localhost:3000"
